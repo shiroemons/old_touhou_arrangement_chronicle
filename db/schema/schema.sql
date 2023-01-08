24694f0f5b3f75a4b228e2e0dd0f3e9818f377bb
shiroemons <shiroemons@gmail.com>
2023-01-08 17:18:18 +0900

    feat: :sparkles: product_distribution_service_urlsとoriginal_song_distribution_service_urlsを追加する

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -48,3 +48,44 @@ comment on column original_songs.is_original is 'オリジナル有無(true: オ
 comment on column original_songs.source_id is '原曲元の原曲ID';
 comment on column original_songs.created_at is '作成日時';
 comment on column original_songs.updated_at is '更新日時';
+
+create type distribution_service as enum (
+    'spotify',
+    'apple_music',
+    'youtube_music',
+    'line_music',
+    'itunes',
+    'youtube',
+    'nicovideo',
+    'sound_cloud'
+);
+
+create table product_distribution_service_urls (
+    id           text                     not null primary key,
+    product_id   text                     not null references products(id),
+    service      distribution_service     not null,
+    url          text                     not null,
+    created_at   timestamp with time zone not null default current_timestamp,
+    updated_at   timestamp with time zone not null default current_timestamp
+);
+comment on table  product_distribution_service_urls is '原作(音楽)配信サービスURL';
+comment on column product_distribution_service_urls.product_id is '原作ID';
+comment on column product_distribution_service_urls.service is '配信サービス';
+comment on column product_distribution_service_urls.url is 'URL';
+comment on column product_distribution_service_urls.created_at is '作成日時';
+comment on column product_distribution_service_urls.updated_at is '更新日時';
+
+create table original_song_distribution_service_urls (
+    id               text                     not null primary key,
+    original_song_id text                     not null references original_songs(id),
+    service          distribution_service     not null,
+    url              text                     not null,
+    created_at       timestamp with time zone not null default current_timestamp,
+    updated_at       timestamp with time zone not null default current_timestamp
+);
+comment on table  original_song_distribution_service_urls is '原曲配信サービスURL';
+comment on column original_song_distribution_service_urls.original_song_id is '原曲ID';
+comment on column original_song_distribution_service_urls.service is '配信サービス';
+comment on column original_song_distribution_service_urls.url is 'URL';
+comment on column original_song_distribution_service_urls.created_at is '作成日時';
+comment on column original_song_distribution_service_urls.updated_at is '更新日時';
